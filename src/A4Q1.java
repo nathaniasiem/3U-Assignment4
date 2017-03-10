e010b2e88bc00a9df518cc9d54dd66f5e7bd7748
simon7323 <simon7323@HRH-IC0034476.wrdsb.ca>
2017-03-10 11:10:50 -0500

    A4Q1 completed with comments

--- src/A4Q1.java
+++ src/A4Q1.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,6 +16,21 @@ public class A4Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create a scanner to scan input
+        Scanner input = new Scanner(System.in);
+        
+        // ask user to type in their name
+        System.out.println("Please enter your name: ");
+        
+        //scanning in their name into a variable 
+        String name2 = input.nextLine();
+        
+        //string name = "Bob";
+        String name = "Bob";
+        
+        //saying hello to Bob and how he is feeling today
+        System.out.println("Hello " + name2 + ".");
+        System.out.println("How are you today? ");
+          
     }
 }
