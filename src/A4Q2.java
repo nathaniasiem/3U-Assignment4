5d42625550ccc5cdcf6c650d91ef7879124d0f24
candy <candy@192.168.0.15>
2017-03-11 10:52:36 -0500

    A4Q2 initial commit

---
+++ src/A4Q2.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author candy
+ */
+public class A4Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner to scan user input
+        Scanner input = new Scanner(System.in);
+        
+        //ask user to type in the measurement.
+        System.out.println("Hello. ");
+        System.out.println("Please enter the measurement in inches you wish to convert: ");
+        int inches = input.nextInt();
+        //convert input to centimetres
+        int convert = inches * 2.54);
+        if (convert > 0);
+        System.out.println("");
+        
+        
+        
+    }
+    
+}
