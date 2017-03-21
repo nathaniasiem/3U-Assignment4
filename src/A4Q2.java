36483e9ff877afba8be8e8ae17d781db575983e8
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-03-21 09:36:01 -0400

    A4Q2 completed with comments

--- src/A4Q2.java
+++ src/A4Q2.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author candy
@@ -19,18 +18,16 @@ public class A4Q2 {
     public static void main(String[] args) {
         // create scanner to scan user input
         Scanner input = new Scanner(System.in);
-        
+
         //ask user to type in the measurement.
+        //use this to convert into centimetres
         System.out.println("Hello. ");
         System.out.println("Please enter the measurement in inches you wish to convert: ");
-        int inches = input.nextInt();
-        //convert input to centimetres
-        int convert = inches * 2.54);
-        if (convert > 0);
-        System.out.println("");
-        
-        
-        
+        double inches = input.nextDouble();
+        //converting input to centimetres
+        double convert = inches * 2.54;
+        if (convert > 0) {
+            System.out.println(inches + " inches " + "is the same as " + convert + "cm ");
+        }
     }
-    
 }
