5c8284e5b2a427d096141d907ae9e92c0f2f0edc
candy <candy@nsiem2k>
2017-03-22 09:07:07 -0400

    A4Q5 finished inputting the marks and test totals

--- src/A4Q5.java
+++ src/A4Q5.java
@@ -20,11 +20,39 @@ public class A4Q5 {
         // create scanner to scan for user input
         Scanner input = new Scanner(System.in);
         
-        //ask user to tpye in their name
+        //ask user to type in their name
         System.out.println("Please enter your name: ");
         //variable for name
         String name = input.nextLine();
-        
+        //Test 1 marks
+        System.out.println("What was the first test out of? ");
+        int test1 = input.nextInt();
+        System.out.println("What mark did you get? ");
+        int mark1 = input.nextInt();
+        //Test 2
+        System.out.println("What was the second test out of? ");
+        int test2 = input.nextInt();
+        System.out.println("What mark did you get? ");
+        int mark2 = input.nextInt();
+        //Test 3
+        System.out.println("What was the third test out of? ");
+        int test3 = input.nextInt();
+        System.out.println("What mark did you get? ");
+        int mark3 = input.nextInt();
+        //Test 4
+        System.out.println("What was the fourth test out of? ");
+        int test4 = input.nextInt();
+        System.out.println("What mark did you get? ");
+        int mark4 = input.nextInt();
+        //Test 5
+        System.out.println("What was the fifth test out of? ");
+        int test5 = input.nextInt();
+        System.out.println("What mark did you get? ");
+        int mark5 = input.nextInt();
+        //determine the test 1 score
+       
+      
+                
         
     }
     
