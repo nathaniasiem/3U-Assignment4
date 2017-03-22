341252a46b6c8865db7b1df116f15214bbb70111
candy <candy@nsiem2k>
2017-03-22 08:53:48 -0400

    A4Q5 initial commit

---
+++ src/A4Q5.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author candy
+ */
+public class A4Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner to scan for user input
+        Scanner input = new Scanner(System.in);
+        
+        //ask user to tpye in their name
+        System.out.println("Please enter your name: ");
+        //variable for name
+        String name = input.nextLine();
+        
+        
+    }
+    
+}
