d6a52b293ec4df325bd76f98a7ac0f0c9b9ede78
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-03-23 10:12:05 -0400

    A4Q7 -initial commit

---
+++ src/A4Q7.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author simon7323
+ */
+public class A4Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner to scan for user input
+        Scanner input = new Scanner(System.in);
+       
+        //create loop
+        //initialize program of game
+        while(true){
+            // ask user to enter sum of dice
+            System.out.println("Enter sum of dice: ");
+            //variable for sum of dice
+            int dice = input.nextInt();
+            //variable for current spot
+            int currentSQ = input.nextInt();
+            //calculate current spot
+            int currentSQ =dice+;
+            //tell user current spot on board
+            System.out.println("You are on square "+currentSQ+".");
+            
+            
+            
+        }
+    }
+}
