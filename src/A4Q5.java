afddc229ab9b1cc7bce7e567753fde678dcc7111
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-03-23 09:23:39 -0400

    A4Q5 completed with comments

--- src/A4Q5.java
+++ src/A4Q5.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author candy
@@ -19,7 +18,7 @@ public class A4Q5 {
     public static void main(String[] args) {
         // create scanner to scan for user input
         Scanner input = new Scanner(System.in);
-        
+
         //ask user to type in their name
         System.out.println("Please enter your name: ");
         //variable for name
@@ -50,30 +49,26 @@ public class A4Q5 {
         System.out.println("What mark did you get? ");
         int mark5 = input.nextInt();
         //determine the test 1 score
-        int tScore1 = mark1*100/test1;
+        int tScore1 = mark1 * 100 / test1;
         //determine the test 2 score
-        double tScore2 = mark2*100/test2;
+        double tScore2 = mark2 * 100.0 / test2;
         //determine the test 3 score
-        double tScore3 = mark3*100/test3;
+        double tScore3 = mark3 * 100.0 / test3;
         //determine the test 4 score
-        double tScore4 = mark4*100/test4;
+        double tScore4 = mark4 * 100.0 / test4;
         //determine the test 5 score
-        double tScore5 = mark5*100/test5;
+        double tScore5 = mark5 * 100.0 / test5;
         //determine the average
-        double avg = (tScore1+tScore2+tScore3+tScore4+tScore5)/5;
-        
-        //
-        System.out.println("Test Scores for "+name);
-        System.out.println("Test 1: "+tScore1 +"%");
-        System.out.println("Test 2: "+tScore2 +"%");
-        System.out.println("Test 3: "+tScore3 +"%");
-        System.out.println("Test 4: "+tScore4 +"%");
-        System.out.println("Test 5: "+tScore5 +"%");
-        System.out.println("Average: "+avg +"%");
-                
-      
-                
-        
+        double avg = (tScore1 + tScore2 + tScore3 + tScore4 + tScore5) / 5.0;
+
+        //Determine the test scores for Doug
+        System.out.println("Test Scores for " + name);
+        System.out.println("Test 1: " + tScore1 + "%");
+        System.out.println("Test 2: " + tScore2 + "%");
+        System.out.println("Test 3: " + tScore3 + "%");
+        System.out.println("Test 4: " + tScore4 + "%");
+        System.out.println("Test 5: " + tScore5 + "%");
+
+        System.out.println("Average: " + avg + "%");
     }
-    
 }
