6d6d462c5667681744839f3396f12875d3f6bb4e
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-03-24 09:50:10 -0400

    A4Q7 -completed with comments

--- src/A4Q7.java
+++ src/A4Q7.java
@@ -27,14 +27,19 @@ public class A4Q7 {
             System.out.println("Enter sum of dice: ");
             //variable for sum of dice
             int dice = input.nextInt();
+            //condition if user inputs sum of dice not between 2-12
             if (dice <= 1) {
                 System.out.println("You quit!");
                 break;
-            }//calculate current position
+            }
+            //calculate current position
             position = position + dice;
             //determine your current position
             System.out.println("You are on square " + position + ".");
             //create conditions when arriving on snake or ladder
+            //at square 54, go down to 19
+            //at square 90, go down to 48
+            //at square 99, go down to 77
             if (position == 54) {
                 position = 19;
                 System.out.println("You hit a snake! Go back to square " + position + ".");
@@ -44,7 +49,11 @@ public class A4Q7 {
             } else if (position == 99) {
                 position = 77;
                 System.out.println("You hit a snake! Go back to square " + position + ".");
-            } else if (position == 9) {
+            } 
+            //at square 9, go up to 34
+            //at square 40, go up to 64
+            //at square 67, go up to 86
+            else if (position == 9) {
                 position = 34;
                 System.out.println("You reached a ladder! Go up to square " + position + ".");
             } else if (position == 40) {
@@ -54,12 +63,14 @@ public class A4Q7 {
                 position = 86;
                 System.out.println("You reached a ladder! Go up to square " + position + ".");
             }
+            //create condition when reaching square 100
+            //if position is at 100, you win
+            //if position is over 100, computer says you already reached 100
             if (position == 100) {
                 System.out.println("You win!");
                 break;
-            }
-            if (dice <= 1) {
-                System.out.println("You quit!");
+            } else if (position >= 100) {
+                System.out.println("You already reached square 100!" + "You win!");
                 break;
             }
         }
